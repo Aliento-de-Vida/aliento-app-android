3a413cdc0ae16f66284fd146381457ce7aa37b72
Pedro Antonio <pedro.gongora@improving.com>
2020-11-14 11:24:58 -0600

    added Hilt

--- app/src/main/java/com/alientodevida/alientoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/alientodevida/alientoapp/ui/home/HomeFragment.kt
@@ -6,27 +6,28 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.alientodevida.alientoapp.R
 import com.alientodevida.alientoapp.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
+    private val homeViewModel by viewModels<HomeViewModel>()
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-
         val binding = FragmentHomeBinding.inflate(layoutInflater)
-
 
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
         })
+
         return binding.root
     }
 }
--- app/src/main/java/com/alientodevida/alientoapp/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/alientodevida/alientoapp/ui/home/HomeViewModel.kt
@@ -1,13 +1,39 @@
 package com.alientodevida.alientoapp.ui.home
 
+import android.os.Parcel
+import android.os.Parcelable
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel @ViewModelInject constructor(
+    
+): ViewModel(), Parcelable {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
+    constructor(parcel: Parcel) : this() {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<HomeViewModel> {
+        override fun createFromParcel(parcel: Parcel): HomeViewModel {
+            return HomeViewModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<HomeViewModel?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
