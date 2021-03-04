6da2d50299026eaa28264c236dd0c21535c23068
Pedro Antonio <pegongorasep@gmail.com>
2021-03-03 18:24:57 -0600

    added social networks

--- app/src/main/java/com/alientodevida/alientoapp/utils/Constants.kt
+++ app/src/main/java/com/alientodevida/alientoapp/utils/Constants.kt
@@ -6,6 +6,16 @@ object Constants {
     val webPageUrl = "https://alientodevida.mx/inicio"
     val ebookDownloadUrl = "https://alientodevida.mx/uploads/ebooks/ebook-adv.pdf"
 
+    // Twitter
+    val TWITTER_USER_ID = "twitter://user?user_id=489975902"
+    val TWITTER_URL = "https://twitter.com/brahamPerez"
+
+    // Instagram
+    val INSTAGRAM_URL = "https://www.instagram.com/abrahampl4"
+
+    // Facebook
+    val FACEBOOK_PAGE_ID = "323027047774785"
+    val FACEBOOK_URL = "https://www.facebook.com/AlientoDeVidaMerida/"
 
     // Spotify
     const val SPOTIFY_TOKEN = "Basic ZTBlNDMyMWYxNTI4NGU5YzkwNzRmMDFjNjAwOTdkOGY6YTQyNjk2MzViYzMyNDkxNjlkNjRhZWYzZTgwNGM1NGM="
@@ -13,7 +23,6 @@ object Constants {
 
     const val SPOTIFY_ARTIST_ID = "4VYxusCiKsWxcfUveymGU5"
     const val PODCAST_ID = "6ib0Zcjx4PwHcn9S15EuAY"
-
 
     // Youtube
     const val YOUTUBE_DEVELOPER_KEY = "AIzaSyD3-lHPYrGTHPUEP_ZpdQEPwx2IXKfznj0"
