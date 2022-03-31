6ea967a8ee243fbc31cac0820245816925a0ad2e
Pedro Antonio <pedro.gongora@delphiansystems.com>
2022-03-31 15:58:59 -0600

    added go to notification detail to home screen

--- app/src/main/java/com/alientodevida/alientoapp/app/features/home/HomeFragment.kt
+++ app/src/main/java/com/alientodevida/alientoapp/app/features/home/HomeFragment.kt
@@ -9,12 +9,14 @@ import com.alientodevida.alientoapp.app.R
 import com.alientodevida.alientoapp.app.base.BaseFragment
 import com.alientodevida.alientoapp.app.compose.theme.AppTheme
 import com.alientodevida.alientoapp.app.databinding.FragmentNotificationsBinding
+import com.alientodevida.alientoapp.app.features.notifications.list.NotificationsFragmentDirections
 import com.alientodevida.alientoapp.app.utils.Utils
 import com.alientodevida.alientoapp.app.utils.extensions.openFacebookPage
 import com.alientodevida.alientoapp.app.utils.extensions.openInstagramPage
 import com.alientodevida.alientoapp.app.utils.extensions.openSpotifyArtistPage
 import com.alientodevida.alientoapp.app.utils.extensions.openTwitterPage
 import com.alientodevida.alientoapp.app.utils.extensions.openYoutubeChannel
+import com.alientodevida.alientoapp.domain.notification.Notification
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -33,6 +35,7 @@ class HomeFragment : BaseFragment<FragmentNotificationsBinding>(R.layout.fragmen
             viewModel = viewModel,
             goToEditHome = { goToEditHome() },
             goToNotifications = { goToNotifications() },
+            goToNotificationDetail = { notification -> goToNotificationDetail(notification)},
             goToSettings = { goToSettings() },
             goToSermons = { goToSermons() },
             goToChurch = { goToChurch() },
@@ -53,114 +56,17 @@ class HomeFragment : BaseFragment<FragmentNotificationsBinding>(R.layout.fragmen
     }  
   }
   
-  /*private var viewListener: ViewListener = ViewListener { position ->
-    val customView = layoutInflater.inflate(R.layout.item_sermons_carrousel, null)
-    
-    (viewModel.sermonsItems.value as? ViewModelResult.Success)?.data?.get(position)?.let {
-      it.imageUrl?.let {
-        customView.findViewById<ImageView>(R.id.imageView).load(it)
-      }
-      
-      when {
-        it.categoryItem != null -> {
-          (customView.findViewById(R.id.play_icon) as ImageView).visibility = View.GONE
-          (customView.findViewById(R.id.triangle) as FrameLayout).visibility = View.GONE
-          (customView.findViewById(R.id.title) as TextView).text = it.title
-          customView.setOnClickListener { goToSermons() }
-        }
-        it.youtubeItem != null -> {
-          (customView.findViewById(R.id.play_icon) as ImageView).visibility = View.VISIBLE
-          (customView.findViewById(R.id.triangle) as FrameLayout).visibility =
-            View.VISIBLE
-          (customView.findViewById(R.id.cl_title) as ConstraintLayout).visibility = View.GONE
-          customView.setOnClickListener { _ ->
-            Utils.handleOnClick(requireActivity(), it.youtubeItem!!.youtubeId)
-          }
-        }
-      }
-      
-    }
-    
-    customView
-  }
-  
-  private fun setupCarousel() {
-    val resourceListener = BaseViewHolder.Listener { item: CarouselItem, _ ->
-      when (item.categoryItem?.type) {
-        CategoryItemType.CHURCH -> goToChurch()
-        CategoryItemType.CAMPUSES -> goToCampus()
-        CategoryItemType.GALLERY -> goToGallery()
-        CategoryItemType.SERMONS -> goToSermons()
-      }
-    }
-  }
-  
-  private fun setupQuickAccess() {
-    with(binding) {
-      donations.setOnClickListener { goToDonations() }
-      prayer.setOnClickListener { goToPrayer() }
-      ebook.setOnClickListener {
-        viewModel.homeResult?.let {
-          Utils.goToUrl(requireContext(), it.home.ebook)
-        }
-      }
-    }
-  }
-  
-  private fun setupSocialMedia() {
-    with(binding) {
-      setupAdminEntrypoint()
-    }
-  }
-  
-  private fun setupAdminEntrypoint() {
-    var twitterLongClickCount = 0
-    var spotifyLongClickCount = 0
-    
-    binding.twitter.setOnLongClickListener {
-      it.performHapticFeedback(
-        HapticFeedbackConstants.VIRTUAL_KEY,
-        HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
-      )
-      twitterLongClickCount++
-      true
-    }
-    
-    binding.spotify.setOnLongClickListener {
-      it.performHapticFeedback(
-        HapticFeedbackConstants.VIRTUAL_KEY,
-        HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
-      )
-      spotifyLongClickCount++
-      if (spotifyLongClickCount == 1 && twitterLongClickCount == 1) {
-        twitterLongClickCount = 0
-        spotifyLongClickCount = 0
-        
-        if (viewModel.isAdmin) {
-          viewModel.signAdminOut()
-          binding.fabEdit.isGone = viewModel.isAdmin.not()
-          showToast(
-            Message.Localized(
-              UUID.randomUUID().mostSignificantBits,
-              Message.Type.INFORMATIONAL,
-              R.drawable.ic_check_48,
-              "Signed Out!",
-              "",
-              "",
-            )
-          )
-        } else {
-          val action = HomeFragmentDirections.actionFragmentHomeToAdminNavigation()
-          findNavController().navigate(action)
-        }
-      }
-      true
-    }
-  }*/
-  
   private fun goToNotifications() {
     val action = HomeFragmentDirections.actionFragmentHomeToNotificationNavigation()
     findNavController().navigate(action)
+  }
+  
+  private fun goToNotificationDetail(notification: Notification) {
+    findNavController().navigate(
+      HomeFragmentDirections.actionFragmentHomeToNotificationDetailFragment(
+        notification
+      )
+    )
   }
   
   private fun goToGallery() {
@@ -236,7 +142,6 @@ class HomeFragment : BaseFragment<FragmentNotificationsBinding>(R.layout.fragmen
       requireActivity().openTwitterPage(socialMedia.twitterUserId, socialMedia.twitterUrl)
     }
   }
-  
   
   private fun goToSpotify() {
     viewModel.viewModelState.value.home?.socialMedia?.spotifyArtistId?.let { spotifyArtistId ->
