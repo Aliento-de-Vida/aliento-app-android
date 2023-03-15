a4e80b22cd746808e0df2f322536a59c765613e4
Pedro Antonio <pegongorasep@gmail.com>
2023-03-15 15:58:51 -0600

    added detekt and lint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,25 +1,27 @@
-import deps.Java
+
 import deps.androidx.AndroidX
-import deps.androidx.Compose
 import deps.config
 import deps.google.Firebase
 import deps.google.Gson
-import deps.google.Hilt
 import deps.jakewharton.RetrofitSerializationConverter
 import deps.jetbrains.Kotlin
 import java.io.FileInputStream
 import java.util.Properties
 
 plugins {
-    id(BuildPlugins.androidApplication)
+    id(BasicPlugin.plugin)
     id(BuildPlugins.googleServices)
-    id(BuildPlugins.kotlinAndroid)
-    id(BuildPlugins.kotlinKapt)
-    id(BuildPlugins.hiltAndroid)
     id(BuildPlugins.safeArgs)
-    id(BuildPlugins.kotlinSerialization)
-    id(BuildPlugins.kotlinParcelize)
     id(BuildPlugins.crashlytics)
+}
+
+android {
+    namespace = "com.alientodevida.alientoapp.app"
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
 }
 
 android {
@@ -29,8 +31,6 @@ android {
 
     defaultConfig {
         applicationId = "com.alientodevida.app"
-        minSdk = config.minSdk
-        targetSdk = config.targetSdk
         versionCode = config.versionCode
         versionName = config.versionName
     }
@@ -51,11 +51,6 @@ android {
 
     buildTypes {
         getByName("release") {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
             buildConfigField(
                 "String",
                 "YOUTUBE_DEVELOPER_KEY",
@@ -64,7 +59,6 @@ android {
             buildConfigField("String", "BASE_URL", "\"https://todoserver-peter.herokuapp.com\"")
         }
         getByName("debug") {
-            isMinifyEnabled = false
             buildConfigField(
                 "String",
                 "YOUTUBE_DEVELOPER_KEY",
@@ -73,40 +67,10 @@ android {
             buildConfigField("String", "BASE_URL", "\"https://todoserver-peter.herokuapp.com\"")
         }
     }
-
-    compileOptions {
-        sourceCompatibility = Java.sourceCompatibility
-        targetCompatibility = Java.targetCompatibility
-    }
-    kotlinOptions {
-        jvmTarget = Java.jvmTarget
-    }
-    kotlin {
-        jvmToolchain {
-            languageVersion.set(JavaLanguageVersion.of(Java.jvmTarget))
-        }
-    }
-    buildFeatures {
-        buildConfig = true
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = Compose.compiler
-    }
-    packagingOptions {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 }
 
 dependencies {
     // Modules
-    implementation(project(":core:domain"))
-    implementation(project(":core:analytics"))
-    implementation(project(":core:presentation"))
-    implementation(project(":core:designsystem"))
-    implementation(project(":core:common"))
     implementation(project(":feature:gallery"))
     implementation(project(":feature:campus"))
     implementation(project(":feature:sermons"))
@@ -128,22 +92,6 @@ dependencies {
     // Appcompat
     implementation("androidx.appcompat:appcompat:${AndroidX.appCompat}")
 
-    // Hilt
-    implementation("com.google.dagger:hilt-android:${Hilt.lib}")
-    kapt("com.google.dagger:hilt-compiler:${Hilt.lib}")
-    implementation("androidx.hilt:hilt-navigation-compose:${AndroidX.hiltNavigation}")
-
-    // Architecture components
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:${AndroidX.lifecycle}")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${AndroidX.lifecycle}")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:${AndroidX.lifecycle}")
-
-    // Compose
-    implementation("androidx.compose.animation:animation:${Compose.lib}")
-    implementation("androidx.compose.ui:ui-tooling:${Compose.lib}")
-    implementation("androidx.compose.runtime:runtime-livedata:${Compose.lib}")
-    implementation("androidx.compose.material:material:${Compose.material}")
-    implementation("androidx.navigation:navigation-compose:${AndroidX.composeNavigation}")
     // Libraries using compose
     implementation("androidx.constraintlayout:constraintlayout-compose:${AndroidX.constraintLayoutCompose}")
     implementation("androidx.activity:activity-compose:${AndroidX.activityCompose}")
